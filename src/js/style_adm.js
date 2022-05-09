37f892ff5486a7b7b2d3e9e31f515f4fbc6e8170
Giovanna Furlan Torres <giovanna.torres@sou.inteli.edu.br>
2022-05-09 11:16:15 -0300

    Update imagens e pasta de documentos

--- src/js/style_adm.js
+++ src/js/style_adm.js
@@ -24,4 +24,84 @@ function habilitarSimQuandoTextInput(){
     }
 }
 
+function habilitarSimTextInput3(){
+    if (document.getElementById("flexRadioDefault3").checked){
+        $("#fieldsetDisabled3").prop("disabled",false);
+    }else{
+        $("#fieldsetDisabled3").prop("disabled",true);
+    }
+}
+
+function habilitarSimTextInput4(){
+    if (document.getElementById("inlineCheckbox1").checked){
+        $("#fieldsetDisabled4").prop("disabled",false);
+    }else{
+        $("#fieldsetDisabled4").prop("disabled",true);
+    }
+}
+
+function habilitarSimTextInput5(){
+    if (document.getElementById("inlineCheckbox2").checked){
+        $("#fieldsetDisabled5").prop("disabled",false);
+    }else{
+        $("#fieldsetDisabled5").prop("disabled",true);
+    }
+}
+
+function habilitarSimTextInput6(){
+    if (document.getElementById("inlineCheckbox3").checked){
+        $("#fieldsetDisabled6").prop("disabled",false);
+    }else{
+        $("#fieldsetDisabled6").prop("disabled",true);
+    }
+}
+
+function habilitarSimTextInput7(){
+    if (document.getElementById("inlineCheckbox4").checked){
+        $("#fieldsetDisabled7").prop("disabled",false);
+    }else{
+        $("#fieldsetDisabled7").prop("disabled",true);
+    }
+}
+
+function habilitarSimTextInput8(){
+    if (document.getElementById("inlineCheckbox5").checked){
+        $("#fieldsetDisabled8").prop("disabled",false);
+    }else{
+        $("#fieldsetDisabled8").prop("disabled",true);
+    }
+}
+
+function habilitarSimTextInput9(){
+    if (document.getElementById("inlineCheckbox6").checked){
+        $("#fieldsetDisabled9").prop("disabled",false);
+    }else{
+        $("#fieldsetDisabled9").prop("disabled",true);
+    }
+}
+
+function habilitarSimTextInput10(){
+    if (document.getElementById("inlineCheckbox7").checked){
+        $("#fieldsetDisabled10").prop("disabled",false);
+    }else{
+        $("#fieldsetDisabled10").prop("disabled",true);
+    }
+}
+
+function habilitarSimTextInput11(){
+    if (document.getElementById("inlineCheckbox8").checked){
+        $("#fieldsetDisabled11").prop("disabled",false);
+    }else{
+        $("#fieldsetDisabled11").prop("disabled",true);
+    }
+}
+
+function habilitarSimTextInput12(){
+    if (document.getElementById("inlineCheckbox9").checked){
+        $("#fieldsetDisabled12").prop("disabled",false);
+    }else{
+        $("#fieldsetDisabled12").prop("disabled",true);
+    }
+}
+
 
