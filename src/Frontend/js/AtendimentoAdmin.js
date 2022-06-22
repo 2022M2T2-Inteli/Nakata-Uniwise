b69b51686c7f47329b5fa2b2736d9c057f2b009e
Giovanna Furlan Torres <giovanna.torres@sou.inteli.edu.br>
2022-06-22 09:22:30 -0300

    Update link side bar

--- src/Frontend/js/AtendimentoAdmin.js
+++ src/Frontend/js/AtendimentoAdmin.js
@@ -1,3 +1,22 @@
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bi-search");
+closeBtn.addEventListener("click", ()=>{
+sidebar.classList.toggle("open");
+menuBtnChange();//calling the function(optional)
+});
+searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+sidebar.classList.toggle("open");
+menuBtnChange(); //calling the function(optional)
+});
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+if(sidebar.classList.contains("open")){
+    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+}else {
+    closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+}
+}
 //PÁGINA DE ATENDIMENTOS
 
 $(document).ready( function() {
@@ -386,24 +405,3 @@ function editAtendimento(id) {
     $('#myModalAttendance10').modal('hide');
     $('#myModalAttendance10').remove();
 };
-
-
-let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bi-search");
-    closeBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("open");
-    menuBtnChange();//calling the function(optional)
-    });
-    searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
-    sidebar.classList.toggle("open");
-    menuBtnChange(); //calling the function(optional)
-    });
-    // following are the code to change sidebar button(optional)
-function menuBtnChange() {
-    if(sidebar.classList.contains("open")){
-        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
-    }else {
-        closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
-    }
-}
--- src/Frontend/js/inicialScript.js
+++ src/Frontend/js/inicialScript.js
@@ -1,17 +1,14 @@
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
 let searchBtn = document.querySelector(".bi-search");
-
 closeBtn.addEventListener("click", ()=>{
 sidebar.classList.toggle("open");
 menuBtnChange();//calling the function(optional)
 });
-
 searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
 sidebar.classList.toggle("open");
 menuBtnChange(); //calling the function(optional)
 });
-
 // following are the code to change sidebar button(optional)
 function menuBtnChange() {
 if(sidebar.classList.contains("open")){
@@ -20,7 +17,6 @@ if(sidebar.classList.contains("open")){
     closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
 }
 }
-
 
 // MARIANA SCRIPT
 
